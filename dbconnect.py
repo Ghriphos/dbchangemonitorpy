a910bffc0db5fab070d6373b310e3fca61f4a196
Ghriphos <luisfernando._alves@hotmail.com>
2022-12-01 11:40:26 -0300

    feat: lineCount is now a function who returns lenght of tables

--- dbconnect.py
+++ dbconnect.py
@@ -3,6 +3,6 @@ import pyodbc
 
 def dbconnection():
     cnxn = pyodbc.connect(
-        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost\SQLEXPRESS08;DATABASE=test;UID=sa;PWD=12we34rt')
+        'DRIVER={ODBC Driver 17 for SQL Server};SERVER=localhost\SQLEXPRESS08;DATABASE=SecullumAcessoNet;UID=sa;PWD=12we34rt')
 
     return cnxn
